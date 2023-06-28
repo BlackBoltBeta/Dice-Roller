9731268fcf66b9531dc64a4f43b66e05ca71629a
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-06-28 17:18:36 +0000

    FeatStyle):Add light/dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,27 @@ let diceSide = document.getElementById('diceSide');
     }
     printNumber(result);
   };
+
+  // Variables from DOM
+const themeButton = document.getElementById("themeButton");
+
+let isLightTheme = true;
+
+// Switch themes
+const switchTheme = () => {
+	const themeStyleSheet = document.getElementById("themeStyleSheet");
+
+	const newStyleSheet = isLightTheme
+		? "assets/css/dark.css"
+		: "assets/css/light.css";
+
+	themeStyleSheet.setAttribute("href", newStyleSheet);
+
+	themeButton.innerHTML = isLightTheme
+		? `<i class="fa-solid fa-sun"></i>`
+		: `<i class="fa-solid fa-moon"></i>`
+
+	isLightTheme = !isLightTheme;
+};
+
+themeButton.addEventListener("click", switchTheme);
